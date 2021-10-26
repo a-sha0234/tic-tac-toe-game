9e68294011d904d62ce0eb4e3876db6b9b15a75c
a-sha0234 <asharma_31@hotmail.com>
2021-10-26 22:54:41 +0100

    js: begin creating the basic js for the gameboard

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 "use strict";
 
+//selectors
+const gboard = document.querySelectorAll(".field");
 
+//handle gameboard
 const gameBoard = (() => {
-    //moddule
-  const board = ["x" , "o", "x" , "o"];
+  //module
+  const board = [];
   return { board };
 })();
 
-const player1 = () =>{
-    //factory
-   const player1 
-   return {player1}
+//handle players turn
+const playerTurn = (() => {
+  const num = 0;
+  return { num };
+})();
+
+//handle players
+const player1 = () => {
+  //factory
+  const player1 = 0;
+  return { player1 };
+};
+
+const player2 = () => {
+  //factory
+  const player2 = 0;
+  return { player2 };
+};
+
+// display board
+function renderBoard() {
+  console.log(gameBoard.board);
 }
 
-const player2 = () =>{
-    //factory
-    const player2 
-    return {player2}
- }
-
- 
+renderBoard();
